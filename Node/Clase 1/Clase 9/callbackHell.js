1541c0bec670a05d23dd1d79a41ddff661f7020e
Luciano <111991734+LuchoDiazz@users.noreply.github.com>
2023-10-23 16:00:58 -0300

    Add files via upload

---
+++ Node/Clase 1/Clase 9/callbackHell.js
@@ -0,0 +1,61 @@
+function hola(nombre, miCallback){
+    setTimeout(function(miCallback) {
+        console.log('Hola '+nombre);
+miCallback();
+    },1000);
+    
+}
+   
+function hablar (callbackHablar){
+    setTimeout(function(){
+        console.log('bla bla bla');
+        callbackHablar();
+    }, 1000);
+}
+
+function adios (nombre, otroCallback){
+
+    setTimeout( function(){
+      console.log('Adios ', +nombre);
+      otroCallback();
+
+
+    },1500);
+}
+// funcion recursiva
+function conversacion(nombre, veces, callback){
+if (veces > 0) {
+ hablar(function() {
+    conversacion(nombre, --veces, callback);
+ });
+} else {
+ callback(nombre, callback);
+}
+
+
+
+//--proceso principal
+
+console.log('iniciando el proceso...');
+hola ('Ariel'), function(nombre){
+
+    conversacion(nombre, 4, function (){
+        console.log('Terminando el proceso...'); 
+    })
+};
+
+//hola('Carlos',function(nombre) {
+  //  hablar( function(){
+    //    hablar( function() {
+      //      hablar( function() {
+        //        hablar( function() {
+        //adios(nombre, function() {
+          //  console.log('Terminando el proceso...');
+
+      //  });
+
+    //});
+   
+//});
+//});
+//});
