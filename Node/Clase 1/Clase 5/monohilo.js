b7b5f34efd4d7ace5a8334e5acbc0f5ff9354583
Luciano <111991734+LuchoDiazz@users.noreply.github.com>
2023-10-23 15:07:42 -0300

    Add files via upload

---
+++ Node/Clase 1/Clase 5/monohilo.js
@@ -0,0 +1,15 @@
+console.log ('Hola a todos de la cohoerte 2023')
+
+var i = 0;
+
+setiInterval(function(){
+console.log(i);
+
+i++;
+if (i ==5){
+    console.log ('Forzamos un error');
+    var = a = 3 + z;
+}
+}, 1000);
+
+console.log('Segunda instruccion');
